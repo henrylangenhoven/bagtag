60cb3aea78d24daf4e9705ff132ff8ad496e5803
Henry Langenhoven <henrylangenhoven8@gmail.com>
2024-04-06 15:56:39 +0200

    added prettier commands and ran prettier on all files

--- configSetup.js
+++ configSetup.js
@@ -1,19 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
-const dir = "src/environments";
-const file = "environment.ts";
-const prodFile = "environment.prod.ts"; // For production deployment
+const dir = 'src/environments';
+const file = 'environment.ts';
+const prodFile = 'environment.prod.ts'; // For production deployment
 
 const content = `${process.env.FIREBASE_CONFIG}`;
 
-fs.access(dir, fs.constants.F_OK, (err) => {
+fs.access(dir, fs.constants.F_OK, err => {
   if (err) {
     // Directory doesn't exist
     console.log("src doesn't exist, creating now", process.cwd());
     // Create /src
     try {
-      fs.mkdirSync(dir, {recursive: true});
+      fs.mkdirSync(dir, { recursive: true });
     } catch (error) {
       console.log(`Error while creating ${dir}. Error is ${error}`);
       process.exit(1);
@@ -21,12 +21,12 @@ fs.access(dir, fs.constants.F_OK, (err) => {
   }
   // Now write to file
   try {
-    fs.writeFileSync(dir + "/" + file, content);
-    fs.writeFileSync(dir + "/" + prodFile, content);
-    console.log("Created successfully in", process.cwd());
-    if (fs.existsSync(dir + "/" + file)) {
-      console.log("File is created", path.resolve(dir + "/" + file));
-      const str = fs.readFileSync(dir + "/" + file).toString();
+    fs.writeFileSync(dir + '/' + file, content);
+    fs.writeFileSync(dir + '/' + prodFile, content);
+    console.log('Created successfully in', process.cwd());
+    if (fs.existsSync(dir + '/' + file)) {
+      console.log('File is created', path.resolve(dir + '/' + file));
+      const str = fs.readFileSync(dir + '/' + file).toString();
       console.log(str);
     }
   } catch (error) {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
-import {Component, inject} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import {Observable} from 'rxjs';
-import {AsyncPipe, NgForOf} from "@angular/common";
-import {FirestoreService} from "./firebase/firestore.service";
+import { Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AsyncPipe, NgForOf } from '@angular/common';
+import { FirestoreService } from './firebase/firestore.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NgForOf, AsyncPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  firestoreService: FirestoreService = inject(FirestoreService)
+  firestoreService: FirestoreService = inject(FirestoreService);
   items$: Observable<any[]>;
 
   constructor() {
