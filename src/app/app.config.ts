c4b73ba73b10c818a1abbbbb5ec469b99544651c
Henry Langenhoven <henrylangenhoven8@gmail.com>
2024-04-06 11:43:32 +0200

    ran `ng add @angular/fire`

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp({
+      "projectId": "bagtag-lang",
+      "appId": "1:256848826132:web:0098f362f4485eea00fc01",
+      "storageBucket": "bagtag-lang.appspot.com",
+      "apiKey": "AIzaSyAEw4HsDXhFtP76drx0Imc-p6VUNOKv22Y",
+      "authDomain": "bagtag-lang.firebaseapp.com",
+      "messagingSenderId": "256848826132",
+      "measurementId": "G-T6LFH84B36"
+    }))),
+    importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
