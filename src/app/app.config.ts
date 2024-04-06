1c6ba43e072c358b3981e7ec699b1ecd609dc8f3
Henry Langenhoven <henrylangenhoven8@gmail.com>
2024-04-06 12:26:17 +0200

    move app config to github secrets

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,17 +4,10 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {environment} from "../environments/environment";
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
-    importProvidersFrom(provideFirebaseApp(() => initializeApp({
-      "projectId": "bagtag-lang",
-      "appId": "1:256848826132:web:0098f362f4485eea00fc01",
-      "storageBucket": "bagtag-lang.appspot.com",
-      "apiKey": "AIzaSyAEw4HsDXhFtP76drx0Imc-p6VUNOKv22Y",
-      "authDomain": "bagtag-lang.firebaseapp.com",
-      "messagingSenderId": "256848826132",
-      "measurementId": "G-T6LFH84B36"
-    }))),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebase))),
     importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
