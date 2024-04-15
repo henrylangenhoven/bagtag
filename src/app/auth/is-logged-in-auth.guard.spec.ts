e2b348cd9c66e7b17d881679668fe11c3281d950
Henry Langenhoven <henrylangenhoven8@gmail.com>
2024-04-15 16:16:33 +0200

    todo authguard tests

--- src/app/auth/is-logged-in-auth.guard.spec.ts
+++ src/app/auth/is-logged-in-auth.guard.spec.ts
@@ -1,18 +1,1 @@
-import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
-
-import { isLoggedInAuthGuard } from './is-logged-in-auth.guard';
-
-describe('isLoggedInAuthGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) =>
-    TestBed.runInInjectionContext(() => isLoggedInAuthGuard(...guardParameters));
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-  });
-
-  it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
-  });
-});
 // TODO
