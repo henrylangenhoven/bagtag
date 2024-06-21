197b172dae34d49cd8a3cbb4169ba224b9043904
Henry Langenhoven <henry.langenhoven.dev@gmail.com>
2024-06-21 13:50:13 +0200

    add environment-template.ts

---
+++ src/environments/environment-template.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'my-api-key',
+    authDomain: 'my-auth-domain',
+    projectId: 'my-project-id',
+    storageBucket: 'my-storage-bucket',
+    messagingSenderId: 'my-messaging-sender-id',
+    appId: 'my-app-id',
+    measurementId: 'my-measurement-id',
+  },
+};
